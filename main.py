d01358cbee2413d28b16d4c917d0a44a5df79d6c
elfaldia <diego.17rojas2002@gmail.com>
2024-09-20 18:30:23 -0300

    a

--- main.py
+++
@@ -1,6 +0,0 @@
-from calc_parser import parse
-
-# Prueba del parser con una expresión simple
-expression = "3 + 5 - 2"
-result = parse(expression)
-print(f"Resultado de '{expression}' es {result}")
